8120847c5683c653b9415c2a51c9ba4a0e984607
ronsha001 <ronsh0111@gmail.com>
2022-07-20 15:26:57 +0300

    Switch base64 images to imageURL with multer

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
-import FileBase from 'react-file-base64';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
@@ -20,17 +19,29 @@ const Form = ({ currentId, setCurrentId }) => {
     useEffect(() => {
         if(post) setPostData(post);
     }, [post])
-
+    // console.log(postData);
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if(currentId) {
-            dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
+            const formData = new FormData();
+            formData.append('title', postData.title);
+            formData.append('message', postData.message);
+            formData.append('tags', postData.tags);
+            formData.append('selectedFile', postData.selectedFile);
+            formData.append('name', user?.result?.name);
+            dispatch(updatePost(currentId, formData));
         } else {
-            dispatch(createPost({ ...postData, name: user?.result?.name }, navigate));
+            const formData = new FormData();
+            formData.append('title', postData.title);
+            formData.append('message', postData.message);
+            formData.append('tags', postData.tags);
+            formData.append('selectedFile', postData.selectedFile);
+            formData.append('name', user?.result?.name);
+            dispatch(createPost(formData, navigate));
         }
         clear();
-    }
+}     
     
     const clear = () => {
         setCurrentId(null);
@@ -54,7 +65,7 @@ const Form = ({ currentId, setCurrentId }) => {
                 <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
                 <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
                 <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}/>
-                <div className={classes.fileInput}><FileBase type='file' multiple={false} onDone={({base64}) => setPostData({...postData, selectedFile: base64})}/></div>
+                <div className={classes.fileInput}><input type='file' multiple={false} onChange={(e) => setPostData({...postData, selectedFile: e.target.files[0]})}/></div>
                 <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth>Submit</Button>
                 <Button variant='contained' color='secondary' size='small' onClick={clear} fullWidth>Clear</Button>
             </form>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,119 +1,165 @@
+import path from "path";
+import fs from "fs";
+
 import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
-import express from 'express';
+import express from "express";
 const router = express.Router();
 export const getPost = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const post = await PostMessage.findById(id);
+  try {
+    const post = await PostMessage.findById(id);
 
-        res.status(200).json(post);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getPosts = async (req, res) => {
-    const { page } = req.query;
-    try{
-        const LIMIT = 8;
-        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
-        const total = await PostMessage.countDocuments({}); // get total posts
+  const { page } = req.query;
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+    const total = await PostMessage.countDocuments({}); // get total posts
 
-        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex); // get the specific starting index posts
-        
-        res.status(200).json({ data: posts, currentPage: (Number(page)), numberOfPages: Math.ceil(total / LIMIT) });
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-}
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex); // get the specific starting index posts
+
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getPostsBySearch = async (req, res) => {
-    const { searchQuery, tags } = req.query
-    
-    try {
-        const title = new RegExp(searchQuery, 'i');
+  const { searchQuery, tags } = req.query;
 
-        const posts = await PostMessage.find({ $or: [ { title }, { tags: { $in: tags.split(',') } } ] });
+  try {
+    const title = new RegExp(searchQuery, "i");
 
-        res.json({ data: posts });
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(",") } }],
+    });
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const createPost = async (req, res) => {
-    const post = req.body;
+  const post = req.body;
+  console.log(post);
+  const newPost = new PostMessage({
+    title: post.title,
+    message: post.message,
+    name: post.name,
+    tags: post.tags.split(','),
+    selectedFile: req.file.path.replace("\\", "/"),
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
+  try {
+    await newPost.save();
 
-    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
-    try{
-        await newPost.save();
-
-        res.status(201).json(newPost);
-
-    } catch (error) {
-        res.status(409).json({message: error.message});
-    }
-}
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = req.params;
-    const post = req.body;
+  const { id: _id } = req.params;
+  const post = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
+  const postToUpdate = await PostMessage.findById(_id);
+  let filePath = postToUpdate.selectedFile;
+  if (req?.file?.path) {
+    filePath = req.file.path.replace("\\", "/");
+    clearImage(postToUpdate.selectedFile);
+  }
+  postToUpdate.title = post.title;
+  postToUpdate.message = post.message;
+  postToUpdate.name = post.name;
+  postToUpdate.tags = post.tags.split(',');
+  postToUpdate.selectedFile = filePath;
+  await postToUpdate.save();
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id }, {new: true});
-
-    res.json(updatedPost);
-}
+  res.json(postToUpdate);
+};
 
 export const deletePost = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
 
-    await PostMessage.findByIdAndRemove(id);
+  const post = await PostMessage.findById(id);
+  if (post.selectedFile) {
+    clearImage(post.selectedFile);
+  }
 
-    res.json({ message: 'Post deleted successfully' });
-}
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: "Post deleted successfully" });
+};
 
 export const likePost = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if(!req.userId) return res.json({ message: 'Unauthenticated' });
+  if (!req.userId) return res.json({ message: "Unauthenticated" });
 
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
 
-    const post = await PostMessage.findById(id);
+  const post = await PostMessage.findById(id);
 
-    const index = post.likes.findIndex((id) => id === String(req.userId));
+  const index = post.likes.findIndex((id) => id === String(req.userId));
 
-    if(index === -1) {
-        // like the post
-        post.likes.push(req.userId);
-    } else {
-        // dislike the post
-        post.likes = post.likes.filter((id) => id !== String(req.userId));
-    }
+  if (index === -1) {
+    // like the post
+    post.likes.push(req.userId);
+  } else {
+    // dislike the post
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
 
-    res.json(updatedPost);
-}
+  res.json(updatedPost);
+};
 
 export const commentPost = async (req, res) => {
-    const { id } = req.params;
-    const { value } = req.body;
+  const { id } = req.params;
+  const { value } = req.body;
 
-    const post = await PostMessage.findById(id);
+  const post = await PostMessage.findById(id);
 
-    post.comments.push(value);
+  post.comments.push(value);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
 
-    res.json(updatedPost);
-    
-}
+  res.json(updatedPost);
+};
+
+const clearImage = (filePath) => {
+  const __dirname = path.dirname("");
+  filePath = path.join(__dirname, filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
 
 export default router;
